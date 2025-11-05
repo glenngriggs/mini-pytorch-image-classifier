cfe8a6e2f31052f428726904e9bb30bbb7aa6f93
glenngriggs <glenngr@umich.edu>
2025-11-04 21:03:49 -0500

    forward

--- models.py
+++ models.py
@@ -10,15 +10,23 @@ class FCNet(nn.Module):
         if activation_function_name == "sigmoid":
             self.activation_function = torch.sigmoid
         # TODO: initialize the layers for the fully-connected neural network (please do not change layer names!)
-        self.linear1 = None
-        self.linear2 = None
-        self.linear3 = None
+        self.linear1 = nn.Linear(3072, 500)
+        self.linear2 = nn.Linear(500, 100)
+        # 10 classes in CIFAR-10
+        self.linear3 = nn.Linear(100, 10)
 
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
         # TODO: complete the forward pass (use self.activation_function)
         
+        # first hidden layer + activation
+        x = self.activation_function(self.linear1(x))
+        # second hidden layer + activation
+        x = self.activation_function(self.linear2(x))
+        # classification layer (no activation here; CrossEntropyLoss in run.py expects logits)
+        x = self.linear3(x)
+
         return x
 
 class ConvNet(nn.Module):
